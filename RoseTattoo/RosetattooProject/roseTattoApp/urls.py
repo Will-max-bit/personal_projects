a848002f465b724020bd997253596640d7be6219
Will-max-bit <whermann4@gmail.com>
2021-04-08 22:22:19 -0700

    Started Tattoo shop page

---
+++ RoseTattoo/RosetattooProject/roseTattoApp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name='roseTattoApp'
+urlpatterns = [
+    path('index/', views.index, name='index'),
+    path('artists_page/', views.artists_page, name='artists_page')
+]
---
+++ RoseTattoo/RosetattooProject/roseTattoApp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'roseTattoApp/index.html')
+
+def artists_page(request):
+    return render(request, 'roseTattoApp/artists_page.html')
