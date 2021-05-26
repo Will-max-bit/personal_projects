17b8395c9b9e560b77968160f70e5ec7465f599d
Will-max-bit <whermann4@gmail.com>
2021-05-25 21:58:40 -0700

    updated models

--- routerater/sendItProject/sendItApp/models.py
+++ routerater/sendItProject/sendItApp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.fields import related
+from django.conf import settings
 
 class UserProfile(models.model):
     profile_picture = models.ImageField(upload_to='images/', null=True, blank=True)
@@ -8,3 +9,5 @@ class UserProfile(models.model):
     def __str__(self) -> str:
         return self.user.username + ' - ' + str(self.profile_picture.name)
 
+class Post(models.model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
