b3582ebfc53092c4a01f62c4fc035be451cda7bf
Will-max-bit <whermann4@gmail.com>
2021-05-26 21:35:14 -0700

    updated models

--- routerater/sendItProject/sendItApp/models.py
+++ routerater/sendItProject/sendItApp/models.py
@@ -1,13 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import constraints
 from django.db.models.fields import related
 from django.conf import settings
+from django.utils import timezone
 
-class UserProfile(models.model):
+class UserProfile(models.Model):
     profile_picture = models.ImageField(upload_to='images/', null=True, blank=True)
-    user = models.models.OneToOneField(User, on_delete=models.PROTECT, related_name='user_profile')
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='user_profile')
+    is_climber = models.BooleanField('climber', default=False)
+    is_setter = models.BooleanField('setter', default=False)
+    is_gym = models.BooleanField('gym', default=False)
     def __str__(self) -> str:
         return self.user.username + ' - ' + str(self.profile_picture.name)
 
-class Post(models.model):
+class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    post_image = models.ImageField(upload_to='images/', null=True, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    likes = models.ManyToManyField(User, related_name='post_likes')
+
+class UserFollowing(models.Model):
+    user_id = models.ForeignKey("User", related_name='following')
+    following_user_id = models.ForeignKey("User", related_name='followers')
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user_id', 'following_user_id'], name="unique_followers")
+        ]
